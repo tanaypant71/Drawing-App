94b7a4a46230e61c3f19b252def6c512f8319679
TANAY PANT <tanaypant71@gmail.com>
2023-05-07 10:19:04 +0530

    colors can be selected

--- app/src/main/java/com/example/kidsapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
