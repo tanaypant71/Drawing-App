81784f0166475b87764456b7c30057e30e304a90
TANAY PANT <tanaypant71@gmail.com>
2023-05-07 23:54:50 +0530

    code completed finally!!!

--- app/src/main/java/com/example/kidsapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsapp/MainActivity.kt
@@ -10,7 +10,14 @@ import android.widget.Toast
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.provider.MediaStore
+import android.provider.Settings
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -18,11 +25,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+    var customProgressDialog: Dialog? = null
 
     val openGalleryLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
@@ -93,6 +109,23 @@ class MainActivity : AppCompatActivity() {
             drawingView?.onClickUndo()
         }
 
+        val saveIt: ImageButton = findViewById(R.id.save_btn)
+        //Adding an click event to save or exporting the image to your phone storage.
+        saveIt.setOnClickListener {
+
+            if (isReadStorageAllowed()){
+                //launch a coroutine block
+                showProgressDialog()
+                lifecycleScope.launch {
+                    //reference the frame layout
+                        val flDrawingView: FrameLayout = findViewById(R.id.frame_container)
+
+                    //Save the image to the device
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+        }
+
         val redoIt: ImageButton = findViewById(R.id.redoIt)
         redoIt.setOnClickListener {
             drawingView?.onClickRedo()
@@ -151,6 +184,19 @@ class MainActivity : AppCompatActivity() {
          }
 
 
+    private fun isReadStorageAllowed(): Boolean {
+        //Getting the permission status
+        // Here the checkSelfPermission is
+
+         //Determine whether you have been granted a particular permission.
+        val result = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        //If permission is granted returning true and If permission is not granted returning false
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestStoragePermission(){
         if (ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
@@ -161,8 +207,8 @@ class MainActivity : AppCompatActivity() {
                     "needs to Access Your External Storage")
         }else{
             requestPermission.launch(arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            //TODO - writing external storage permission
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
             ))
         }
     }
@@ -179,5 +225,130 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         builder.create().show()
+    }
+
+
+    //Sharing the downloaded Image file
+    private fun shareImage(result:String){
+        /*scanFile is used to scan the file when the connection is established with MediaScanner.*/
+        MediaScannerConnection.scanFile(
+            this@MainActivity, arrayOf(result), null
+        ) { path, uri ->
+            // This is used for sharing the image after it has being stored in the storage.
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(
+                Intent.EXTRA_STREAM,
+                uri
+            ) // A content: URI holding a stream of data associated with the Intent, used to supply the data being sent.
+            shareIntent.type =
+                "image/png" // The MIME type of the data being handled by this intent.
+            startActivity(
+                Intent.createChooser(
+                    shareIntent,
+                    "Share"
+                )
+            )
+        }
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+
+        //Define a bitmap with the same size as the view.
+        // CreateBitmap : Returns a mutable bitmap with the
+        // specified width and height
+        val returnedBitmap = Bitmap.createBitmap(view.width,
+            view.height, Bitmap.Config.ARGB_8888)
+
+        //Bind a canvas to it
+        val canvas = Canvas(returnedBitmap)
+
+        //Get the view's background
+        val bgDrawable = view.background
+        if (bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        // draw the view on the canvas
+        view.draw(canvas)
+
+        //return the bitmap
+        return  returnedBitmap
+    }
+
+    // A method to save the image.
+    private  suspend fun saveBitmapFile(mBitmap: Bitmap?): String  {
+        var result = ""
+        withContext(Dispatchers.IO){
+            if (mBitmap != null){
+                try {
+                    val bytes = ByteArrayOutputStream() // Creates a new byte array output stream.
+
+                    // compresses the bitmap image to PNG format with a quality of 90% and
+                    // writes it to an output stream called bytes
+                    mBitmap.compress(Bitmap.CompressFormat.PNG,90, bytes)
+
+                    val file = File(externalCacheDir?.absoluteFile.toString()
+                                   + File.separator + "KidsApp_" + System.currentTimeMillis() /1000 + ".png"
+                    )
+
+
+                    //creates a file output stream ,
+                    // writes bytes from the specified byte array ,
+                    //close the file output stream & release all the system resources
+                    val fout = FileOutputStream(file)
+                    fout.write(bytes.toByteArray())
+                    fout.close()
+
+                    // The file absolute path is return as a result.
+                    result = file.absolutePath
+
+                    //We switch from io to ui thread to show a toast
+                    runOnUiThread {
+                        cancelProgressDialog()
+                        if (result.isNotEmpty()){
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File saved successfully :$result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            shareImage(result)
+                        }else{
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong while saving the file.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+                catch (e: Exception){
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        return result
+    }
+
+    private fun showProgressDialog() {
+        customProgressDialog = Dialog(this@MainActivity)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+
+        //Start the dialog and display it on screen.
+        customProgressDialog?.show()
+    }
+
+    private fun cancelProgressDialog() {
+        if (customProgressDialog != null) {
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
+        }
     }
 }
