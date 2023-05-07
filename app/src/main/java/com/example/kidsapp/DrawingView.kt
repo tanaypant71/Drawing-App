e5910f27b15e99b0d98cd0565bbcf9615fa717fd
TANAY PANT <tanaypant71@gmail.com>
2023-05-07 15:17:14 +0530

    applied undo and redo app is working fine..

--- app/src/main/java/com/example/kidsapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsapp/DrawingView.kt
@@ -17,10 +17,25 @@ class DrawingView(context: Context,attrs: AttributeSet): View(context,attrs) {
     private var color = Color.BLACK
     private var canvas: Canvas? = null
     private var mPaths = ArrayList<CustomPath>()
+    private var mUndoPaths = ArrayList<CustomPath>()
+    private var mRedoPaths = ArrayList<CustomPath>()
+
 
 
     init {
         setUpDrawing()
+    }
+
+    fun onClickUndo(){
+        if (mPaths.size > 0){
+            mUndoPaths.add(mPaths.removeAt(mPaths.size-1))
+            invalidate()
+        }
+    }fun onClickRedo(){
+        if (mUndoPaths.size > 0){
+            mPaths.add(mUndoPaths.removeAt(mUndoPaths.size-1))
+            invalidate()
+        }
     }
 
 
--- app/src/main/java/com/example/kidsapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.kidsapp
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -14,8 +24,46 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
 
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if (result.resultCode == RESULT_OK && result.data != null){
+                val imageBackGround: ImageView = findViewById(R.id.im_bg)
 
+                imageBackGround.setImageURI(result.data?.data)
+            }
+        }
 
+    @SuppressLint("SuspiciousIndentation")
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach {
+                    val permissionName = it.key
+                    val isGranted = it.value
+                    if (isGranted) {
+                        //check the permission name and perform the specific operation
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Permission granted for storage files",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        val pickIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                            openGalleryLauncher.launch(pickIntent)
+                    }
+                    else{
+                        if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Permission not granted for storage files",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,6 +81,21 @@ class MainActivity : AppCompatActivity() {
         val brush: ImageButton = findViewById(R.id.brush)
         brush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+
+        val myGallery: ImageButton = findViewById(R.id.gallery)
+        myGallery.setOnClickListener {
+            requestStoragePermission()
+        }
+
+        val undoIt: ImageButton = findViewById(R.id.undo)
+        undoIt.setOnClickListener {
+            drawingView?.onClickUndo()
+        }
+
+        val redoIt: ImageButton = findViewById(R.id.redoIt)
+        redoIt.setOnClickListener {
+            drawingView?.onClickRedo()
         }
 
     }
@@ -87,4 +150,34 @@ class MainActivity : AppCompatActivity() {
             }
          }
 
+
+    private fun requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            //call the rationale dialog to tell the user why they need to allow permission request
+            showRationaleDialog("Kids Drawing App","Kids Drawing App " +
+                    "needs to Access Your External Storage")
+        }else{
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            //TODO - writing external storage permission
+            ))
+        }
+    }
+
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
 }
